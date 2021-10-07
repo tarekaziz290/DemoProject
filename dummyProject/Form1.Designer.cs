f26775b59d358a1cd32d7eda5f3558e6e32fa8f5
tarekaziz290 <62283332+tarekaziz290@users.noreply.github.com>
2021-10-07 13:49:49 +0600

    update

--- dummyProject/Form1.Designer.cs
+++ dummyProject/Form1.Designer.cs
@@ -44,18 +44,18 @@
             // btnNext
             // 
             this.btnNext.BackColor = System.Drawing.Color.Silver;
-            this.btnNext.Font = new System.Drawing.Font("SketchFlow Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNext.Location = new System.Drawing.Point(12, 296);
             this.btnNext.Name = "btnNext";
             this.btnNext.Size = new System.Drawing.Size(75, 23);
             this.btnNext.TabIndex = 0;
-            this.btnNext.Text = "Next";
+            this.btnNext.Text = "Welcome";
             this.btnNext.UseVisualStyleBackColor = false;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // cmbSelect
             // 
-            this.cmbSelect.Font = new System.Drawing.Font("Georgia", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbSelect.FormattingEnabled = true;
             this.cmbSelect.Items.AddRange(new object[] {
             "Location",
@@ -70,9 +70,9 @@
             "BirthCertificate",
             "Passport",
             "DrivingLicence"});
-            this.cmbSelect.Location = new System.Drawing.Point(93, 91);
+            this.cmbSelect.Location = new System.Drawing.Point(99, 91);
             this.cmbSelect.Name = "cmbSelect";
-            this.cmbSelect.Size = new System.Drawing.Size(121, 22);
+            this.cmbSelect.Size = new System.Drawing.Size(121, 21);
             this.cmbSelect.TabIndex = 1;
             this.cmbSelect.SelectedIndexChanged += new System.EventHandler(this.cmbSelect_SelectedIndexChanged);
             // 
@@ -80,18 +80,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.ForestGreen;
-            this.label1.Location = new System.Drawing.Point(1, 93);
+            this.label1.Location = new System.Drawing.Point(2, 94);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Select Content";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Font = new System.Drawing.Font("Lucida Calligraphy", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmit.Location = new System.Drawing.Point(109, 129);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(75, 23);
@@ -104,10 +104,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label2.Font = new System.Drawing.Font("Papyrus", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(258, 2);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 21);
+            this.label2.Size = new System.Drawing.Size(83, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Total Point";
             // 
@@ -123,7 +123,7 @@
             // btnCalculator
             // 
             this.btnCalculator.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.btnCalculator.Font = new System.Drawing.Font("Buxton Sketch", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculator.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalculator.Location = new System.Drawing.Point(12, 255);
             this.btnCalculator.Name = "btnCalculator";
             this.btnCalculator.Size = new System.Drawing.Size(75, 23);
@@ -135,12 +135,12 @@
             // btnAd
             // 
             this.btnAd.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.btnAd.Font = new System.Drawing.Font("MV Boli", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAd.Location = new System.Drawing.Point(13, 214);
             this.btnAd.Name = "btnAd";
-            this.btnAd.Size = new System.Drawing.Size(75, 23);
+            this.btnAd.Size = new System.Drawing.Size(80, 23);
             this.btnAd.TabIndex = 7;
-            this.btnAd.Text = "AD free";
+            this.btnAd.Text = "SlideShow";
             this.btnAd.UseVisualStyleBackColor = false;
             this.btnAd.Click += new System.EventHandler(this.btnAd_Click);
             // 
@@ -148,10 +148,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.GreenYellow;
-            this.label3.Font = new System.Drawing.Font("Lucida Sans", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(2, 182);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 15);
+            this.label3.Size = new System.Drawing.Size(116, 16);
             this.label3.TabIndex = 8;
             this.label3.Text = "Token  features";
             // 
@@ -159,10 +159,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.label4.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(9, 9);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1, 3);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(166, 21);
+            this.label4.Size = new System.Drawing.Size(142, 20);
             this.label4.TabIndex = 9;
             this.label4.Text = "Value Generator";
             // 
